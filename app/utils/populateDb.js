2a4d2d902232078e034f3ca700e350aa09f3c596
Nassim Vachor <nassim-vachor@hotmail.fr>
2016-11-16 15:50:30 +0100

    all create function for db

--- app/utils/populateDb.js
+++ app/utils/populateDb.js
@@ -5,7 +5,10 @@ import {
   createUser,
   createSellingCompany,
   createProduct,
-  createCommand
+  createCommand,
+  createJournal,
+  createEntry,
+  createObjective
 } from "../persistence/db"
 
 initDb(config.DB_URL)
@@ -119,7 +122,69 @@ let commands = [
     ],
   }
 ]
-
+let journals = [
+  {
+    userId: 1,
+    id: 1,
+    title: 'Projet AIOP',
+    creationDate: new Date(2016,11,16),
+  },
+  {
+    userId: 2,
+    id:2,
+    title: 'Stage IG5',
+    creationDate: new Date(2016,11,15),
+  },
+  {
+    userId: 3,
+    id: 3,
+    title: 'Java',
+    creationDate: new Date(2016,11,14),
+  }
+]
+let entries = [
+  {
+    journalId: 1,
+    id: 1,
+    description: 'bientôt les soutenances',
+    ressourceType: 'text',
+    ressourceUrl: '',
+    creationDate: new Date(2016,11,14),
+  },
+  {
+    journalId: 1,
+    id: 2,
+    description: 'finir le DAT pour ce soir',
+    ressourceType: 'text',
+    ressourceUrl: '',
+    creationDate: new Date(2016,11,15),
+  },
+  {
+    journalId: 2,
+    id: 3,
+    description: 'je dois trouver un stage',
+    ressourceType: 'text',
+    ressourceUrl: '',
+    creationDate: new Date(2016,11,15),
+  }
+]
+let objectives = [
+  {
+    journalId: 1,
+    id: 1,
+    description: 'avoir une bonne note en AIOP',
+  },
+  {
+    journalId: 2,
+    id: 2,
+    description: 'Stage avec pre-embauche',
+  },
+  {
+    journalId: 3,
+    id: 3,
+    description: 'integrer java community',
+  }
+]
 console.log("clear database");
 clearDb()
 .then(() => {
@@ -140,10 +205,28 @@ clearDb()
   console.log("create commands");
   //return Promise.resolve()
   return Promise.all(commands.map(c => createCommand(c.idUser, c.id, c.products)))
-
+  console.log("commands created");
 })
 .then(() => {
-  console.log("commands created");
+  console.log("create journals");
+  //return Promise.resolve()
+  return Promise.all(journals.map(c => createJournal(c.userId, c.id, c.title, c.creationDate)))
+  console.log("journals created");
+})
+.then(() => {
+  console.log("create entries");
+  //return Promise.resolve()
+  return Promise.all(entries.map(c => createEntry(c.journalId, c.id, c.description, c.ressourceType, c.ressourceUrl, c.creationDate)))
+  console.log("entries created");
+})
+.then(() => {
+  console.log("create objectives");
+  //return Promise.resolve()
+  return Promise.all(objectives.map(c => createObjective(c.journalId, c.id, c.description)))
+  console.log("objectives created");
+})
+.then(() => {
+  console.log("all it's right");
   return Promise.resolve()
 })
 .then(() => {
