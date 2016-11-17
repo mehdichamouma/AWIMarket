d396694e9e92c88393ad5a8e0744618da424d0be
Arnaud Zaragoza <zaragoza.arnaud@gmail.com>
2016-11-17 23:24:05 +0100

    connect all the service route files to the main route file and write all the route for the journals service

--- app/web/api/index.js
+++ app/web/api/index.js
@@ -1,10 +1,18 @@
 import express from "express"
 import journals from "./journals.web"
 import auth from "./authentification.web"
+import users from "./users.web"
+import products from "./products.web"
+import commands from "./commands.web"
+import notifications from "./notifications.web"
 
 let api = express.Router()
 
 api.use("/journals", journals)
 api.use("/auth", auth)
+api.use("/users", users)
+api.use("/products", products)
+api.use("/commands", commands)
+api.use("/notifications", notifications)
 
 export default api
--- app/web/api/journals.web.js
+++ app/web/api/journals.web.js
@@ -3,6 +3,8 @@ import {getJournalsByUser, createJournal} from "../../services/journals"
 
 let router = express.Router()
 
+// all journals
+
 router.get("/", (req, res) => {
   let {userId} = req.query
   getJournalsByUser(userId)
@@ -27,5 +29,58 @@ router.post("/", (req, res) => {
     .send(result.description);
   })
 })
+
+// one journal
+
+router.get("/journals/:journalId", (req, res) => {
+  res.status(501)
+  .send("Not Implemented")
+}
+
+router.put("/journals/:journalId", (req, res) => {
+  res.status(501)
+  .send("Not Implemented")
+}
+
+router.delete("/journals/:journalId", (req, res) => {
+  res.status(501)
+  .send("Not Implemented")
+}
+
+// all entries
+
+router.get("/journals/:journalId/entries", (req, res) => {
+  res.status(501)
+  .send("Not Implemented")
+}
+
+router.post("/journals/:journalId/entries", (req, res) => {
+  res.status(501)
+  .send("Not Implemented")
+}
+
+// one entries
+
+router.put("/journals/:journalId/entries/:entryId", (req, res) => {
+  res.status(501)
+  .send("Not Implemented")
+}
+
+router.delete("/journals/:journalId/entries/:entryId", (req, res) => {
+  res.status(501)
+  .send("Not Implemented")
+}
+
+// comments
+
+router.get("/journals/:journalId/comments", (req, res) => {
+  res.status(501)
+  .send("Not Implemented")
+}
+
+router.delete("/journals/:journalId/comments/:commentId", (req, res) => {
+  res.status(501)
+  .send("Not Implemented")
+}
 
 export default router
