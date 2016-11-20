a708ca7385075255f218a98959684854bf3a347c
Arnaud Zaragoza <zaragoza.arnaud@gmail.com>
2016-11-20 16:41:37 +0100

    Add rights in products service

--- app/services/products/index.js
+++ app/services/products/index.js
@@ -2,6 +2,7 @@ import getDB from "../../persistence"
 
 let productsService = {}
 
+// Access SC
 productsService.getProducts = (params) => {
   if(params.keywords != undefined) {
     return productsService.getProductsByKeywords(params.keywords)
@@ -12,6 +13,7 @@ productsService.getProducts = (params) => {
   })
 }
 
+// Access : Everybody
 productsService.getProductsByKeywords = (keywords) => {
   if(keywords != undefined && keywords instanceof String) {
     return getDB().getProductsByKeywords(keywords)
@@ -22,30 +24,35 @@ productsService.getProductsByKeywords = (keywords) => {
   return Promise.reject({code:400, description:"Bad Request"})
 }
 
+
+// Access : SC
 productsService.createProducts = (req) => {
   let data = req.body
   let user = req.user
-  if(user != null && user.roles) {
-    console.log("User is log");
-  }
-
-  if(data.product != undefined) {
-    let p = data.product
-    if(p.sellingCompany != undefined && p.sellingCompany instanceof String &&
-        p.name != undefined && p.name instanceof String &&
-        p.description != undefined && p.description instanceof String &&
-        p.price != undefined && p.price instanceof Number &&
-        p.quantity != undefined && p.quantity instanceof Number
-      ) {
-      return getDB().createProduct(p.sellingCompany, undefined, p.name, p.description, p.price, p.quantity)
-      .catch((error) => {
-        return Promise.reject({code:500, description:"Server error"})
-      })
+  if(user != null && user.roles.includes("SELLING_COMPANY_OWNER")) {
+    if(data.product != undefined) {
+      let p = data.product
+      if(p.sellingCompany != undefined && p.sellingCompany instanceof String &&
+          p.name != undefined && p.name instanceof String &&
+          p.description != undefined && p.description instanceof String &&
+          p.price != undefined && p.price instanceof Number &&
+          p.quantity != undefined && p.quantity instanceof Number
+        ) {
+        return getDB().getUser(user.id)
+        .then((result) => {
+          return getDB().createProduct(result.company.id, undefined, p.name, p.description, p.price, p.quantity)
+        })
+        .catch((error) => {
+          return Promise.reject({code:500, description:"Server error"})
+        })
+      }
     }
+    return Promise.reject({code:400, description:"Bad Request"})
   }
-  return Promise.reject({code:400, description:"Bad Request"})
+  return Promise.reject({code:401, description:"Unauthorized"})
 }
 
+// Access : Everybody
 productsService.getProduct = (productId) => {
   if(typeof productId === "string" || productId instanceof String) {
     return getDB().getProduct(productId)
@@ -56,23 +63,38 @@ productsService.getProduct = (productId) => {
   return Promise.reject({code:400, description:"Bad Request"})
 }
 
-productsService.updateProduct = (productId, data) => {
-  if(productId instanceof String && data.product != undefined) {
-    let p = data.product
-    if(p.sellingCompany != undefined && p.sellingCompany instanceof String &&
-        p.name != undefined && p.name instanceof String &&
-        p.description != undefined && p.description instanceof String &&
-        p.price != undefined && p.price instanceof Number &&
-        p.quantity != undefined && p.quantity instanceof Number
-      ) {
-      return getDB().updateProduct(p.sellingCompany, productId, p.name, p.description, p.price, p.quantity)
-      .catch((error) => {
-        return Promise.reject({code:500, description:"Server error"})
+// Access : SellingComany
+productsService.updateProduct = (productId, req) => {
+  let data = req.body
+  let user = req.user
+  if(user != null && user.roles.includes("SELLING_COMPANY_OWNER")) {
+    if(productId instanceof String && data.product != undefined) {
+      let p = data.product
+      if(p.name != undefined && p.name instanceof String &&
+          p.description != undefined && p.description instanceof String &&
+          p.price != undefined && p.price instanceof Number &&
+          p.quantity != undefined && p.quantity instanceof Number
+        ) {
+          return getDB().getUser(user.id)
+          .then((result) => {
+            return getDB().getProduct(productId)
+            .then((product) => {
+              if(product.id == result.company.id) {
+                return getDB().updateProduct(result.company.id, productId, p.name, p.description, p.price, p.quantity)
+                .catch((error) => {
+                  return Promise.reject({code:500, description:"Server error"})
+                })
+              }
+            })
+          })
+        .catch((error) => {
+          return Promise.reject({code:500, description:"Server error"})
+        })
       }
-      )
     }
+    return Promise.reject({code:400, description:"Bad Request"})
   }
-  return Promise.reject({code:400, description:"Bad Request"})
+  return Promise.reject({code:401, description:"Unauthorized"})
 }
 
 productsService.deleteProduct = (productId) => {
